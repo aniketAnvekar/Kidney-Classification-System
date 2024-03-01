e9cb537e0a7c6cab807010e9873d4609620daff3
aniketAnvekar <aniketanvekar25@gmail.com>
2024-02-29 23:22:39 -0500

    Added modular code files for Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from CNN_classifier import logger
+from CNN_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our application Kidney Disease classification")
+STAGE_NAME = 'Data Ingestion stage'
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ research/trial.py
@@ -0,0 +1,5 @@
+from CNN_classifier.constants import *
+from CNN_classifier.utils.common import read_yaml, create_directories
+
+
+print(read_yaml(CONFIG_FILE_PATH))
